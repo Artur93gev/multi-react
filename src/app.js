e31428adc25a59fdca5f0d35382214d42628d5ef
artur93gev <artur93gev@gmail.com>
2018-11-26 23:15:38 +0400

    inital repo

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        multi react app
+      </div>
+    );
+  }
+}
+
+export default App;
