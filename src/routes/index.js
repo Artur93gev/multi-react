1e1748fd125f6303ad6ee6bdce08cf3eb83c6033
artur93gev <artur.gevorgyan@betconstruct.com>
2018-11-27 11:21:33 +0400

    adding routes to project

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from 'pages/home';
+import DynamicImporter from 'components/unControlled/dynamicImporter';
+
+const Configs = _ => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/first" render={props => <DynamicImporter {...props} componentName="first" />} />
+    <Route exact path="/second" render={props => <DynamicImporter {...props} componentName="second" />} />
+  </Switch>
+);
+
+export default Configs;
