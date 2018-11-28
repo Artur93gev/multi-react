8cd568ca715632301c9141bf41cc35c82a4af9ef
artur93gev <artur.gevorgyan@betconstruct.com>
2018-11-28 11:27:31 +0400

    dynamicImporter component implemantaion changed for bootstraping apps

--- src/components/unControlled/dynamicImporter/index.js
+++ src/components/unControlled/dynamicImporter/index.js
@@ -6,17 +6,33 @@ import { routeEnums } from 'constants/enums';
 
 class DynamicImporter extends React.PureComponent {
 
-  static containerId = 'container'
+  static containerId = 'container';
 
   componentDidMount() {
+    this.bootstrap();
+  }
+
+  bootstrap() {
     const Application = React.lazy(_ => import(`applications/${this.props.componentName}`));
 
     configs.routePrefix = `/${routeEnums[this.props.componentName]}`;
 
-    bootstrap(Application, document.getElementById(DynamicImporter.containerId));
+    bootstrap(Application, DynamicImporter.containerId);
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    if (prevProps.componentName !== this.props.componentName) {
+      this.bootstrap();
+    }
+    return null;
+  }
+
+  componentDidUpdate() {
+
   }
 
   render() {
+
     return (
       <div id="container">
       </div>
