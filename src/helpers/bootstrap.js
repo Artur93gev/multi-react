d95d7cb2b312dd08b8943fae4cbc397ef366fd8d
artur93gev <artur.gevorgyan@betconstruct.com>
2018-11-28 11:26:25 +0400

    adding cache to bootstrap function

--- src/helpers/bootstrap.js
+++ src/helpers/bootstrap.js
@@ -2,23 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Loader from 'components/unControlled/loader';
 
-const bootstrap = (Application, container, afterMount = console.log) => {
+const cache = {};
 
+const bootstrap = (Application, containerId, afterMount = console.log) => {
   // unmounting component that exists in the container if there is any
   const Component = (
     <React.Suspense fallback={<Loader />}>
       <Application />
     </React.Suspense>
   );
-  if (ReactDOM.unmountComponentAtNode(container)) {
-    console.info('Container has been cleared!!!');
-  }
 
-  // NOTE. new VDOM will be created for mounting component
-  ReactDOM.render(Component, container, () => {
-    console.log('%cNew component has been mounted successfully', 'color: green');
-    afterMount();
-  });
+  const oldContainer = cache.containerId || document.getElementById(containerId);
+
+  if (oldContainer) {
+    
+    if (ReactDOM.unmountComponentAtNode(oldContainer)) {
+      console.info('Container has been cleared!!!');
+    }
+
+    const newContainer = document.getElementById(containerId);
+
+    // NOTE. new VDOM will be created for mounting component
+    return ReactDOM.render(Component, newContainer, () => {
+      cache.containerId = newContainer;
+      afterMount();
+      console.log('%cNew component has been mounted successfully', 'color: green');
+    });
+  }
 };
 
 export default bootstrap;
