3ad000a52204a6bbee6c108ae5714064eda533a7
artur93gev <artur.gevorgyan@betconstruct.com>
2018-11-30 12:07:28 +0400

    adding first application bootstrap script

---
+++ src/first.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import First from './applications/first';
+import './styles.scss';
+
+ReactDOM.render(<First />, document.getElementById('root'));
